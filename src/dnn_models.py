404ce729b426920d3c25afac905922e467bf82d0
dlolis <dlolis88@gmail.com>
2022-05-26 15:25:35 +0300

    embedding code complete

--- src/dnn_models.py
+++ src/dnn_models.py
@@ -29,13 +29,12 @@ def train_epoch(_epoch, dataloader, model, loss_function, optimizer, ev_tr):
         # 2 - forward pass: compute predicted y by passing x to the model
         outputs = model(inputs)
         # 3 - compute loss
-        loss = loss_function(outputs, labels.float())
+        loss = loss_function(outputs.squeeze(), labels.float().squeeze())
         if ev_tr == "train":
             loss.backward()
 
             # 5 - update weights
             optimizer.step()
-
         loss_score.append(loss.detach().item())
     return np.average(loss_score)
 
@@ -43,29 +42,47 @@ def train_epoch(_epoch, dataloader, model, loss_function, optimizer, ev_tr):
 class SampleDataset(Dataset):
 
     def __init__(self, path):
-        features = ['CountryPlayer', 'RoundCount', 'Turnover']
-        labels = ['GGR']
-        pass
+        self.features = ['CountryPlayer', 'RoundCount', 'Turnover']
+        self.labels = ['GGR']
+        self.path = path
 
-    def __getitem(self):
-        pass
+    def __getitem__(self, id):
+        chunk = pd.read_csv(self.path, chunksize=100000)
+        ft = ""
+        lb = ""
+        for temp in chunk:
+            res = temp.loc[temp.index.isin([id]), :]
+            if res.shape[0]:
+                ft = res.loc[:, self.features]
+                lb = res.loc[:, self.labels]
+                ft, lb = (res.index[0], torch.FloatTensor(ft.values)), torch.FloatTensor(lb.values)
+                break
+        return ft, lb
 
     def __len__(self):
-        pass
+        return pd.read_csv(self.path).shape[0]
 
 
 class EmdeddingTrainer(nn.Module):
 
-    def __init__(self, total_players, embedding_dim=50, kernel_sizes=(30, 40, 50), output_size=1):
+    def retrieve_embedding(self, item):
+        search_emb = torch.tensor(item, dtype=torch.long)
+        if torch.cuda.is_available():
+            search_emb = search_emb.cuda()
+        return self.embs(search_emb)
+
+    def __init__(self, total_players, embedding_dim=50, kernel_sizes=(3, 4, 5), output_size=1):
         super(EmdeddingTrainer, self).__init__()
-        self.embs = Embedding(len(total_players), embedding_dim=embedding_dim+3)
+        self.embs = Embedding(total_players, embedding_dim=embedding_dim)
         self.embs.weight.requires_grad = True
-        self.convs = nn.ModuleList([nn.Conv1d(1, embedding_dim+3, (K, 1)) for K in kernel_sizes])
-        self.fc = nn.Linear(len(kernel_sizes) * embedding_dim+3, output_size)
+        self.convs = nn.ModuleList([nn.Conv1d(1, embedding_dim+3, K) for K in kernel_sizes])
+        self.fc = nn.Linear(len(kernel_sizes) * embedding_dim+3*len(kernel_sizes), output_size)
 
     def forward(self, x):
-        inputs = x.unsqueeze(1).unsqueeze(-1)
-        inputs = [torch.relu(conv(inputs)).squeeze(3) for conv in self.convs]  # [(N,Co,W), ...]*len(Ks)
+        idx, x = x
+        retrieved_emb = self.retrieve_embedding(idx)
+        x = torch.cat([retrieved_emb.unsqueeze(1), x], axis=2)
+        inputs = [torch.relu(conv(x)) for conv in self.convs]  # [(N,Co,W), ...]*len(Ks)
         inputs = [torch.max_pool1d(i, i.size(2)).squeeze(2) for i in inputs]  # [(N,Co), ...]*len(Ks)
         concated = torch.cat(inputs, 1)
         out = self.fc(concated)
@@ -73,7 +90,7 @@ class EmdeddingTrainer(nn.Module):
 
 
 BATCH = 10000
-EPOCHS = 20
+EPOCHS = 5
 lr = 0.001
 
 X_path = "../data/player_features.csv"
@@ -82,20 +99,16 @@ model = EmdeddingTrainer(pd.read_csv(X_path).shape[0])
 criterion = torch.nn.L1Loss()
 
 train_dataset = SampleDataset(X_path)
-train_loader = DataLoader(train_dataset, batch_size=BATCH, shuffle=True, num_workers=0)
+train_loader = DataLoader(train_dataset, batch_size=BATCH, shuffle=True, num_workers=4)
 parameters = filter(lambda p: p.requires_grad, model.parameters())
 
 optimizer = torch.optim.Adam(parameters, lr=lr)
 
 train_losses = []
-eval_losses = []
 
-train_scores = []
-eval_scores = []
 
 for i in range(EPOCHS):
+    print(f"epoch {i}")
     train_loss = train_epoch(i, train_loader, model, criterion, optimizer, "train")
     train_losses.append(train_loss)
-
-print("best evaluation scores are on {} {}".format(max(eval_scores), np.argmax(eval_scores)))
-print("best model loss score is {} on {} epoch".format(min(eval_losses), np.argmin(eval_losses)))
+    print(f"train loss {train_loss}")
